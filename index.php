b595c51477a192495153baef24f4ff1b904af21b
Titovskyi <titovskyi@gmail.com>
2017-12-07 14:30:04 +0200

    send to github

---
+++ index.php
@@ -0,0 +1,220 @@
+<!--СДЕЛАТЬ ПАРАЛАКС НА БАННЕР, поправить мобильную версию-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Nikolai Portnoi</title>
+    <link rel="shortcut icon" href="img/loyer.jpg" type="image/x-icon">
+	<link rel="stylesheet" href="stylesheets/fonts.css">
+	<link rel="stylesheet" href="stylesheets/font-awesome.css">
+	<link rel="stylesheet" href="stylesheets/normalize.css">
+	<link rel="stylesheet" href="stylesheets/style.css">
+</head>
+<body>
+	<header>
+	   <a id="#top"></a>
+		<div class="banner">
+			<div class="main">
+				<h1>Николай Портной</h1><p>Адвокат</p>
+				<div class="header_menu" id="menu">
+					<ul>
+					    <li><a href="#about">Об адвокате</a></li>
+						<li><a href="#services">Услуги</a></li>
+						<li><a href="#news">Преимущества</a></li>
+						<li><a href="#map">Контакты</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
+	<section class="content">
+		<div class="about">
+		<a id="about"></a>
+			<h2>Об Адвокате</h1>
+			<p>Я предлагаю качественное решение Ваших проблем в сфере законодательства Украины опытным адвокатом. Главная цель моей работы - это достижение наилучшего результата для клиентов путем предоставления профессиональной юридической помощи в решении любых вопросов. Квалифицированная юридическая консультация поможет скоординировать действия клиентов в любой ситуации, помощь и сопровождение в до-судовом процессе, а так же участие в судах по гражданским, уголовным, хозяйственным и административным делам гарантирует максимальное удовлетворение потребностей клиента.</p>
+			<img src="img/foto.png" alt="loyer">
+		</div>
+		<div class="services">
+		<a id="services"></a>
+			<h2>Услуги</h2>
+			<div class="tabbed">
+				<input type="radio" name="tabs" id="tab-nav-1" checked>
+				<label for="tab-nav-1">Хозяственное право</label>
+				<input type="radio" name="tabs" id="tab-nav-2">
+				<label for="tab-nav-2">Гражданское право</label>
+				<input type="radio" name="tabs" id="tab-nav-3">
+				<label for="tab-nav-3">Уголовный процесс</label>
+				<input type="radio" name="tabs" id="tab-nav-4">
+				<label for="tab-nav-4">Административное право</label>
+				<input type="radio" name="tabs" id="tab-nav-5">
+				<label for="tab-nav-5">Семейное право</label>
+				<input type="radio" name="tabs" id="tab-nav-6">
+				<label for="tab-nav-6">Жилищное право</label>
+				<input type="radio" name="tabs" id="tab-nav-7">
+				<label for="tab-nav-7">Наследственное право</label>
+				<input type="radio" name="tabs" id="tab-nav-8">
+				<label for="tab-nav-8">Трудовые споры</label>
+				<input type="radio" name="tabs" id="tab-nav-9">
+				<label for="tab-nav-9">Банковские споры</label>
+				<div class="tabs">
+					<div>
+						<span>Хозяственное право</span>
+						<ul>
+							<li>Регистрация ООО, СПД;</li>
+							<li>Юридическое сопровождение предпринимательской деятельности ФЛП и хозяйственной деятельности юридических лиц;</li>
+							<li>Представление юридических лиц в судах, органах местного самоуправления, силовых структурах, ГФС, ГИС;</li>
+							<li>Защита предприятий от выемок, обысков, проверок;</li>
+							<li>Корпоративные споры;</li>
+							<li>Анализ и подготовка договоров;</li>
+							<li>Участие в переговорах;</li>
+							<li>Внесение изменений в уставные документы;</li>
+							<li>Защита чести, достоинства и деловой репутации.</li>
+						</ul>
+					</div>
+					<div>
+						<span>Гражданское право</span>
+						<ul>
+							<li>Взыскание материального и морального ущерба;</li>
+							<li>Признание сделок недействительными;</li>
+							<li>Признание права собственности на имущество;</li>
+							<li>Споры с юридическими лицами;</li>
+							<li>Защита прав потребителя;</li>
+							<li>Страховые выплаты;</li>
+							<li>Возмещении материального вреда причиненного имуществу;</li>
+							<li>Взыскание ущерба после ДТП.</li>
+						</ul>
+					</div>
+					<div>
+						<span>Уголовный процесс</span>
+						<ul>
+							<li>Защита в уголовному процессе на любой его стадии.</li>
+						</ul>
+					</div>
+					<div>
+						<span>Административное право</span>
+						<ul>
+							<li>Обжалование решение органов местного управления;</li>
+							<li>Обжалование действий исполнительной службы;</li>
+							<li>Обжалование решений фискальной службы.</li>
+						</ul>
+					</div>
+					<div>
+						<span>Семейное право</span>
+						<ul>
+							<li>Расторжении брака;</li>
+							<li>Взыскание алиментов;</li>
+							<li>Усыновление и опека над близкими;</li>
+							<li>Раздел имущества супругов;</li>
+							<li>Установление факта родственных отношений.</li>
+						</ul>
+					</div>
+					<div>
+						<span>Жилищное право</span>
+						<ul>
+							<li>Вселение;</li>
+							<li>Лишение права пользования жильем;</li>
+							<li>Взыскание ущерба нанесенного затоплением;</li>
+							<li>Обжалование сумм, начисленных ЖКХ.</li>
+						</ul>
+					</div>
+					<div>
+						<span>Наследственное право</span>
+						<ul>
+							<li>Вступление в наследство;</li>
+							<li>Помощь в оформлении документов;</li>
+							<li>Признание завещания недействительным;</li>
+							<li>Восстановление сроков на принятие наследства</li>
+							<li>Споры с наследниками.</li>
+						</ul>
+					</div>
+					<div>
+						<span>Трудовые споры</span>
+						<ul>
+							<li>Обжалование увольнения;</li>
+							<li>Обжалования дисциплинарного взыскания;</li>
+							<li>Изменение формулировки увольнения.</li>
+						</ul>
+					</div>
+					<div>
+						<span>Банковские споры</span>
+						<ul>
+							<li>Кредитные споры;</li>
+							<li>Обжалование решений суда на стадии апелляционного и кассационного обжалования;</li>
+							<li>Полная конфиденциальность;</li>
+							<li>В случае необходимости - оказание своевременной помощи в день обращения, в любой точке города без перерывов и выходных.</li>
+						</ul>
+					</div>
+				</div>
+				</div>
+			</div>
+
+		<div class="advantages">
+		<a id="news"></a>
+			<h2>Преимущества</h2>
+			<div class="adv_list">
+				<ul>
+					<li>Решение юридических вопросов наиболее приемлемым для Вас способом.</li>
+					<li>Возможность компенсировать в судебном процессе вложенные средства.</li>
+					<li>Индивидуальный подход к каждому Клиенту.</li>
+					<li>Гибкая ценовая политика.</li>
+					<li>Достижение необходимого результата.</li>
+					<li>Полная конфиденциальность.</li>
+					<li>Абонентское обслуживание.</li>
+					<li>Возможность безналичного расчета.</li>
+					<li>Для Вашего удобства возможен выезд в удобное для Вас место.</li>
+				</ul>
+			</div>
+			<div class="news">
+				<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fn.i.portnoy%2Fposts%2F816884231796148&width=500" width="500" height="539" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+				<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fn.i.portnoy%2Fposts%2F815730288578209&width=500" width="500" height="539" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+			</div>
+		</div>
+		<div class="map">
+		<a id="map"></a>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d796.2101856115788!2d33.33727063716157!3d47.83724602860721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40db27c8d8ab0719%3A0xab01d3fc60a3b097!2z0J_QvtGI0YLQvtCy0LAg0LLRg9C70LjRhtGPLCAxLCDQmtGA0LjQstC40Lkg0KDRltCzLCDQlNC90ZbQv9GA0L7Qv9C10YLRgNC-0LLRgdGM0LrQsCDQvtCx0LvQsNGB0YLRjA!5e0!3m2!1sru!2sua!4v1493407308106" ></iframe>
+			<div class="form" id="form">
+				<h3>Связаться</h3>
+				<div class="messages"></div>
+				<p><label><input type="text" name="name" size="30" id="user_name" maxlength="20" placeholder="Ваше имя" tabindex="1" required></label></p>
+				<p><input type="email" name="email" size="30" id="user_email" maxlength="20" placeholder="Ваш email" tabindex="2" required></p>
+				<textarea name="mes" cols="32" rows="5" id="text_comment" placeholder="Ваше сообщение" required ></textarea>
+				<input id="send_button" type="button" value="Отправить"/>
+				<p>г. Кривой Рог, ул. Карла Маркса, 1</p>
+				<a href="tel:+7 (097) 888 30 30">097-888-30-30</a>
+				<p>ПОЧТА</p>
+			</div>
+		</div>
+		<footer>Copyright Here 2017</footer>
+	</section>
+	<div id="topbut"></div><a href="#top" title="Вернуться к началу" class="topbutton"><i class="fa fa-arrow-up" aria-hidden="true"></i></a></div>
+    
+</body>
+	<script src="js/jquery-3.2.1.js"  type="text/javascript"></script>
+	<script src="js/anchor_link.js"  type="text/javascript"></script>
+	<script>
+		$(document).ready(function(){
+			$('#send_button').click(function(){
+				// собираем данные с формы
+				var user_name 	 = $('#user_name').val();
+				var user_email 	 = $('#user_email').val();
+				var text_comment = $('#text_comment').val();
+				// отправляем данные
+				$.ajax({
+					url: "send.php", // куда отправляем
+					type: "post", // метод передачи
+					dataType: "json", // тип передачи данных
+					data: { // что отправляем
+						"user_name": 	user_name,
+						"user_email": 	user_email,
+						"text_comment": text_comment
+					},
+					// после получения ответа сервера
+					success: function(data){
+						$('.messages').html(data.result); // выводим ответ сервера
+					}
+				});
+			});
+		});
+	</script>
+</html>
